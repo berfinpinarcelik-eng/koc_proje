11693ebe28ce92864a601ec9f7e674e0fca59208
berfinpinarcelik-eng <berfinpinarcelik@gmail.com>
2026-05-01 14:51:30 +0300

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ else:
 
 try:
           with open("index.html", "r", encoding="utf-8") as f:
-                        html_code = f.read()
-                    components.html(html_code, height=1000, scrolling=True)
+                            html_code = f.read()
+                        components.html(html_code, height=1000, scrolling=True)
 except Exception as e:
     st.error(f"Error loading HTML: {e}")
